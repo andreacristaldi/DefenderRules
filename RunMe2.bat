21a0fc9174ae0485363fb558cf4b2c55078133b4
Andrea Cristaldi <68824666+andreacristaldi@users.noreply.github.com>
2025-07-24 20:24:22 +0200

    runme

---
+++ RunMe2.bat
@@ -0,0 +1,75 @@
+@echo off
+setlocal
+
+REM Check if the folder exists
+if not exist "%folder_path%1" (
+    echo The folder does not exist. Creating...
+    mkdir "%folder_path1%"
+    echo Folder successfully created.
+) else (
+    echo The folder already exists.
+)
+
+echo The folder already exists.
+
+DefenderRules.exe mpasbase.vdm.decompressed "%folder_path1%"
+
+set "folder_path2=output_mpasdlta"
+
+REM Check if the folder exists
+if not exist "%folder_path%2" (
+    echo The folder does not exist. Creating...
+    mkdir "%folder_path2%"
+    echo Check if the folder exists
+) else (
+    echo The folder already exists.
+)
+
+echo The folder already exists.
+
+DefenderRules.exe mpasdlta.vdm.decomprressed "%folder_path2%"
+
+
+
+set "folder_path3=output_mpavbase"
+
+REM Check if the folder exists
+if not exist "%folder_path%3" (
+    echo La cartella non esiste. Creazione in corso...
+    mkdir "%folder_path3%"
+    echo Check if the folder exists
+) else (
+    echo The folder already exists.
+)
+
+echo Recovering rules...
+
+DefenderRules.exe mpavbase.vdm.decompressed "%folder_path3%"
+
+
+set "folder_path4=output_mpavdlta"
+
+REM Check if the folder exists
+
+if not exist "%folder_path%4" (
+    echo The folder does not exist. Creation in progress...
+    mkdir "%folder_path4%"
+    echo Folder successfully created.
+) else (
+    echo The folder already exists.
+)
+
+echo Recovering rules...
+
+DefenderRules.exe mpavdlta.vdm.decompressed "%folder_path4%"
+
+
+echo Operation completed.
+echo Use DefenderRuleParser to view the rule details. 
+
+endlocal
+
+
+
+
+
