49def87315857445c5e19602b77db081a2adb611
Andrea Cristaldi <68824666+andreacristaldi@users.noreply.github.com>
2025-07-29 15:25:03 +0200

    Update RunMe2.bat

--- RunMe2.bat
+++ RunMe2.bat
@@ -4,7 +4,7 @@ setlocal
 set "folder_path1=output_mpasbase"
 
 REM Check if the folder exists
-if not exist "%folder_path%1" (
+if not exist "%folder_path1%" (
     echo The folder does not exist. Creating...
     mkdir "%folder_path1%"
     echo Folder successfully created.
@@ -19,7 +19,7 @@ DefenderRules.exe mpasbase.vdm.decompressed "%folder_path1%"
 set "folder_path2=output_mpasdlta"
 
 REM Check if the folder exists
-if not exist "%folder_path%2" (
+if not exist "%folder_path2%" (
     echo The folder does not exist. Creating...
     mkdir "%folder_path2%"
     echo Check if the folder exists
@@ -36,7 +36,7 @@ DefenderRules.exe mpasdlta.vdm.decomprressed "%folder_path2%"
 set "folder_path3=output_mpavbase"
 
 REM Check if the folder exists
-if not exist "%folder_path%3" (
+if not exist "%folder_path3%" (
     echo The folder does not exist. Creating...
     mkdir "%folder_path3%"
     echo Check if the folder exists
@@ -53,7 +53,7 @@ set "folder_path4=output_mpavdlta"
 
 REM Check if the folder exists
 
-if not exist "%folder_path%4" (
+if not exist "%folder_path4%" (
     echo The folder does not exist. Creating...
     mkdir "%folder_path4%"
     echo Folder successfully created.
